d0632507672736d19055384fe4c59544944061d8
onlynyang <chica1875@gmail.com>
2024-07-28 15:12:33 +0900

    Feat: Calendar 모델 재구성 및 시리얼라이저 수정

--- calen/models.py
+++ calen/models.py
@@ -1,19 +1,23 @@
+# calendar/models.py
+from django.contrib.auth.models import User
 from django.db import models
-from user.models import User
 from routine.models import Routine
 
-class Calendar(models.Model):
+class UserRoutine(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    title = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.date} - {self.title}"
-
-class CalendarRoutine(models.Model):
-    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
     routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, default='Pending')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    completed = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.calendar.user.username} - {self.routine.title} - {self.status}"
+class PersonalSchedule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    completed = models.BooleanField(default=False)
+
+class MonthlyTitle(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    month = models.DateField()
+    title = models.CharField(max_length=200)
