5a4f0b12943a471619daa6d9cd082e3953b1f6e9
pyeree <dlagusdn0218@gmail.com>
2024-07-29 03:19:39 +0900

    Feat: 테마페이지 불러오기

--- search/urls.py
+++ search/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import SearchViewSet
+from .views import SearchViewSet,ThemeDetailViewSet
 from rest_framework import routers
 
 from django.conf import settings
@@ -9,6 +9,8 @@ app_name = 'search'
 
 default_router = routers.SimpleRouter(trailing_slash=False)
 default_router.register(r'search', SearchViewSet, basename='search')
+default_router.register(r'theme', ThemeDetailViewSet, basename='theme-detail')
+
 urlpatterns = [
     path('', include(default_router.urls)),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
