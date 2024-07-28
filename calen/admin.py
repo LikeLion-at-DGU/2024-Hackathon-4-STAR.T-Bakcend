f0044efb48101732f7272ea651e83637e81a8e3a
onlynyang <chica1875@gmail.com>
2024-07-28 22:56:07 +0900

    Feat: 업데이트 사항 어드민 등록

--- calen/admin.py
+++ calen/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserRoutine, PersonalSchedule, MonthlyTitle
 
 # Register your models here.
+
+admin.site.register(UserRoutine)
+admin.site.register(PersonalSchedule)
+admin.site.register(MonthlyTitle)
