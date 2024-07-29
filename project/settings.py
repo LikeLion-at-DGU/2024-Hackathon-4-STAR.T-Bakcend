06269739ff12ab22c8bb1f75593570d243494220
pyeree <dlagusdn0218@gmail.com>
2024-07-29 11:34:09 +0900

    Chore:Add requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'allauth',
-        'allauth.account',
+    'allauth.account',
     'allauth.socialaccount',
     # allauth - google
     'allauth.socialaccount.providers.google',
