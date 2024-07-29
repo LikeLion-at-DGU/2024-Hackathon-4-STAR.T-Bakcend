69acaa648543b7f586a1f3ebe4b644a5aeb6ff44
onlynyang <chica1875@gmail.com>
2024-07-29 13:00:13 +0900

    Feat: 소셜로그인-카카오 기능 개발

---
+++ accounts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+# from .models import User
+# from allauth.socialaccount.models import SocialApp
+
+# admin.site.register(SocialApp)
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     pass
--- rank/models.py
+++ rank/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from user.models import User
+from accounts.models import User
 from celeb.models import Celeb
 
 class Rank(models.Model):
