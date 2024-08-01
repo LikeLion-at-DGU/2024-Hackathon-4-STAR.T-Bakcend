cf1edbc14a7ba4eeb80fb5b534c217f395123e6c
pyeree <dlagusdn0218@gmail.com>
2024-08-01 23:06:08 +0900

    Fix: 마이페이지 데이터 값 반환

--- rank/serializers.py
+++ rank/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import CelebScore
+from celeb.serializers import CelebSerializer
+
 
 class CelebScoreSerializer(serializers.ModelSerializer):
+    celeb = CelebSerializer()
     class Meta:
         model = CelebScore
-        fields = '__all__'
+        fields = ['id','score','celeb']
