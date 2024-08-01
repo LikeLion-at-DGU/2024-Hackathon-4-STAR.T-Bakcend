9d08cf7f64da09b8a7410a50585f232998a2fd07
onlynyang <chica1875@gmail.com>
2024-08-01 19:25:46 +0900

    Fix: 셀럽페이지 response 값 재배열

--- celeb/models.py
+++ celeb/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from routine.models import Routine
 
 class Celeb(models.Model):
     name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
     photo = models.URLField(max_length=300)
+    routines = models.ManyToManyField(Routine, related_name='celebrities', blank=True)
 
     def __str__(self):
         return self.name
