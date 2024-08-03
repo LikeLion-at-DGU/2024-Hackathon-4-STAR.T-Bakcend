fdbec139c0153cab88d77b709308ba9aaed9cbbc
pyeree <dlagusdn0218@gmail.com>
2024-08-03 18:41:12 +0900

    Fix : 테마 모델 이미지 필드 수정

--- search/models.py
+++ search/models.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class Theme(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.URLField(max_length=500,max_length=500,null=True, blank=True,default='https://cdn.pixabay.com/photo/2020/08/22/12/36/yoga-5508336_1280.png')
+    image = models.URLField(max_length=500,null=True, blank=True,default='https://cdn.pixabay.com/photo/2020/08/22/12/36/yoga-5508336_1280.png')
 
     def __str__(self):
         return self.title
