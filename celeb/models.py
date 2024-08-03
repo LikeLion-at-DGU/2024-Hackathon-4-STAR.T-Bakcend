a7bc441900a75ac1d45ae38dc99112c30f11dcb7
pyeree <dlagusdn0218@gmail.com>
2024-08-03 16:07:40 +0900

    Fix: 셀럽 모델 사진 필드 글자수 제한 수정

--- celeb/models.py
+++ celeb/models.py
@@ -4,7 +4,7 @@ from routine.models import Routine
 class Celeb(models.Model):
     name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
-    photo = models.URLField(max_length=300, default='https://cdn.pixabay.com/photo/2020/08/22/12/36/yoga-5508336_1280.png')
+    photo = models.URLField(max_length=500, default='https://cdn.pixabay.com/photo/2020/08/22/12/36/yoga-5508336_1280.png')
     routines = models.ManyToManyField(Routine, related_name='celebrities', blank=True)
 
 
