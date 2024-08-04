46bc10767ac98b8309e537ce1748cacb730cf565
onlynyang <chica1875@gmail.com>
2024-08-04 15:38:27 +0900

    Fix: 체크스타로 통일

--- calen/urls.py
+++ calen/urls.py
@@ -7,12 +7,13 @@ router.register(r'calendar', CalendarViewSet, basename='calendar')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('calendar/monthly/<str:month>/', CalendarViewSet.as_view({'get': 'monthly', 'post':'monthly', 'patch':'monthly'}), name='calendar-monthly'),
-
     path('calendar/daily/<str:date>/', CalendarViewSet.as_view({'get': 'daily', 'post': 'create_schedule', 'patch': 'update_schedule'}), name='calendar-daily'),
-
     path('calendar/daily/<str:date>/delete/<int:id>/', CalendarViewSet.as_view({'delete': 'delete_daily'}), name='calendar-daily-personalschedule-delete'),
     path('add_routine/<int:id>/', CalendarViewSet.as_view({'post': 'add_routine'}), name='add-routine'),
-    path('calendar/check_star/<str:month>/', CalendarViewSet.as_view({'get': 'check_star'}), name='check_star'),
+    # path('calendar/check_star/<str:month>/', CalendarViewSet.as_view({'get': 'check_star'}), name='check_star'),
     path('calendar/daily/<str:date>/update_routine/', UpdateRoutineCompletionView.as_view(), name='update-routine'),
+    # 날짜별 요청
+    path('calendar/check_star/date/<str:date>/', CalendarViewSet.as_view({'get': 'check_star'}), name='check_star_date'),
+    # 월별 요청
+    path('calendar/check_star/month/<str:month>/', CalendarViewSet.as_view({'get': 'check_star'}), name='check_star_month'),
 ]
