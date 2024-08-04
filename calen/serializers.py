f6b78bf1a253c7c360b54e5ec28317e36c0e6ee3
onlynyang <chica1875@gmail.com>
2024-08-04 13:26:43 +0900

    Fix: 날짜 get에서 루틴 포함 스케쥴 리퀘스트 바디 형식 완벽

--- calen/serializers.py
+++ calen/serializers.py
@@ -34,11 +34,6 @@ class PersonalScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonalSchedule
         fields = '__all__'
-    
-    # def create(self, validated_data):
-    #     user = self.context['request'].user
-    #     validated_data['user'] = user
-    #     return super().create(validated_data)
 
 class MonthlyTitleSerializer(serializers.ModelSerializer):
     class Meta:
