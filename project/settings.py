ffd39681b561094e1174bbf3447211b797c4d830
pyeree <dlagusdn0218@gmail.com>
2024-08-05 19:05:06 +0900

    Feat: 로그 추가

--- project/settings.py
+++ project/settings.py
@@ -246,3 +246,22 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny' # 디버깅용
     ],
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
